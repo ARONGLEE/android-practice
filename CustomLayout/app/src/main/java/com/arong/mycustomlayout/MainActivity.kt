2c5549a75c7623a4f58afb12844d3bf2e9e433d9
AYOUNGLEE <arong16@naver.com>
2022-01-28 11:49:02 +0900

    [add] 레이아웃을 상속하여 새로운 뷰 정의

---
+++ CustomLayout/app/src/main/java/com/arong/mycustomlayout/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.arong.mycustomlayout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.part1.view.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        part1.imageView.setImageResource(R.mipmap.ic_launcher)
+        part1.textView.setText("홍길동")
+        part1.textView2.setText("010-1234-1234")
+
+        button.setOnClickListener {
+            part1.imageView.setImageResource(R.drawable.image1)
+        }
+        button2.setOnClickListener {
+            part1.imageView.setImageResource(R.drawable.image2)
+        }
+    }
+}
