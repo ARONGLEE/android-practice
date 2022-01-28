a2f06bf8cebe79d47b4d5215d0df2d1a9b86a838
AYOUNGLEE <arong16@naver.com>
2022-01-28 11:01:15 +0900

    [add] 코틀린 유용한 함수들

--- MyBlock/app/src/main/java/com/arong/block/MainActivity.kt
+++ MyBlock/app/src/main/java/com/arong/block/MainActivity.kt
@@ -28,7 +28,55 @@ class MainActivity : AppCompatActivity() {
                 showToast("새로 만든 사람-> ${info}")
             }
        }
+        withButton.setOnClickListener {
+            val person1 = Person("홍길동", 20, "010-1234-1234")
+            with(person1){
+                showToast("이름 : $name")
+                showToast("나이: $age")
+                showToast("전화번호: $mobile")
+            }
+        }
+        letButton.setOnClickListener {
+            var person1:Person? = null
+            var created = false
+            if (created) {
+                person1 = Person("홍길동", 20, "010-1234-1234")
+            }
+
+            var info = person1?.toString()
+            showToast("사람: $info")
+
+            person1?.let {
+                showToast("이름: ${it.name}")
+                showToast("나이: ${it.age}")
+                showToast("모바일: ${it.mobile}")
+            } ?: run {
+                showToast("사람이 null입니다.")
+            }
+        }
+        runButton.setOnClickListener {
+            val person1 = Person("홍길동", 20, "010-1234-1234")
+            val result: Boolean = person1.run {
+                if(name != null){
+                    showToast("이름: $name")
+                    true
+                } else {
+                    false
+                }
+            }
+            showToast("결과: ${result}")
+        }
+        alsoButton.setOnClickListener {
+            val person1 = Person()
+            val person2 = person1.also {
+                if(it.name == null) {
+                    showToast("name 속성 값이 null입니다")
+                    it.name = "홍길동"
+                }
+            }
+        }
     }
+
     fun showToast(message: String) {
         Toast.makeText(this, message, Toast.LENGTH_LONG).show()
     }
